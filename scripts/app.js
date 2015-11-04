e0e6de291258185a6659f3b166bf1e8d6ab7968f
mrmooby <scotthayward@gmail.com>
2015-11-04 10:51:22 -0500

    rough build, working

--- scripts/app.js
+++ scripts/app.js
@@ -1,55 +1,58 @@
 $(document).ready(function(){
 
-// Capture the users home and current address
+  // Capture the users home and current address
 	
 	$('.location-getter').submit(function(event){
 		event.preventDefault();	
 		var homeAddress = $(this).find("input[name='home-address']").val();
 		var currentLocation = $(this).find("input[name='current-location']").val();
+    var geocoder = new google.maps.Geocoder();
+
 		console.log(homeAddress, currentLocation);
 
 		// Request latitude and longitude for home address from google maps geocoding api	
+    if (geocoder){
+      geocoder.geocode( { 'address': homeAddress}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK){
+         //location of first address (latitude + longitude)
+         location1 = results[0].geometry.location;
+        } else {
+         alert("Geocode was not successful for the following reason: " + status);
+        }
+      });
 
-		var homeUrl ="https://maps.googleapis.com/maps/api/geocode/json?address=" + homeAddress +"&key=AIzaSyDGyza7mSup_IeTPg8YHiasaPBAaUUMKBM"
-  		
-  		$.getJSON(homeUrl, function(home){ // function(response) -- naming the object that is returned from the API
-    		console.log(home);
-    		homeLat = home.results[0].geometry.location.lat;
-    		homeLong = home.results[0].geometry.location.lng;
-    		console.log(homeLat, homeLong);
-    	});
-
-		// Request latitude and longitude for current location from google maps geocoding api
-
-		var currentUrl ="https://maps.googleapis.com/maps/api/geocode/json?address=" + currentLocation +"&key=AIzaSyDGyza7mSup_IeTPg8YHiasaPBAaUUMKBM"
-  		
-  		$.getJSON(currentUrl, function(current){ // function(response) -- naming the object that is returned from the API
-    		console.log(current);
-    		currentLat = current.results[0].geometry.location.lat;
-    		currentLong = current.results[0].geometry.location.lng;
-    		console.log(currentLat, currentLong);    		
-    	});
-
-		// Calculate the distance between the two locations using the haversine formula
-
-    function calculateDistance() {
-
-      Number.prototype.toRad = function() {
-        return this * Math.PI / 180;
-      }
-
-      var R = 6371; // km 
-      var x1 = currentLat - homeLat;
-      var dLat = x1.toRad();  
-      var x2 = currentLong - homeLong;
-      var dLon = x2.toRad();  
-      var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-                      Math.cos(homeLat.toRad()) * Math.cos(currentLat.toRad()) * 
-                      Math.sin(dLon/2) * Math.sin(dLon/2);  
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-      var d = R * c; 
-      console.log('distance in km:', d);
+      geocoder.geocode( { 'address': currentLocation}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          //location of second address (latitude + longitude)
+          location2 = results[0].geometry.location;
+          // calling the showMap() function to create and show the map
+          //showMap();
+        } else {
+            alert("Geocode was not successful for the following reason: " + status);
+        }
+          distanceBetween = google.maps.geometry.spherical.computeDistanceBetween(location1, location2) / 1000;
+          console.log(distanceBetween);
+      });
     }
-    
-	});
+  });
 });
+
+
+// Calculate the distance between the two locations using the haversine formula.  NOTE: because the earth is an oblate
+// spheroid and not a perfect sphere (which this formula assumes), the result produced by the formula is rather inaccurate.  
+// It's also more code, so I'll just use the google API call in the final version.  Will include this in one last commit for 
+// posterity/learning purposes.
+//
+//  function toRad(deg) {
+//    return deg * Math.PI/180;
+//  }
+//
+//  var R = 6371;
+//  var dLat = toRad(location2.lat()-location1.lat());
+//  var dLon = toRad(location2.lng()-location1.lng());
+//  var dLat1 = toRad(location1.lat());
+//  var dLat2 = toRad(location2.lat());
+//  var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(dLat1) * Math.cos(dLat1) * Math.sin(dLon/2) * Math.sin(dLon/2);
+//  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+//  var d = R * c;
+//  console.log(d);
